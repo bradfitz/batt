e464d7ab67aa28c82ea5392c5f2e8c70acd67b5f
Brad Fitzpatrick <brad@danga.com>
2012-04-23 23:12:25 +0000

    fix

--- util.go
+++ util.go
@@ -11,7 +11,7 @@ type Message struct {
 }
 
 func (m Message) String() string {
-	return fmt.Sprintf("%v %v", m.Verb, m.Values)
+	return fmt.Sprintf("%v %v", m.Verb, m.Values.Encode())
 }
 
 func ParseMessage(s string) (m Message, err error) {
