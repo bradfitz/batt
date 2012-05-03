50891f9c6185f3f4a6c5aaf2e985591f59919cb2
Andrew Gerrand <adg@golang.org>
2012-05-03 11:55:22 +1000

    build multiple platforms

--- battc/battc.go
+++ battc/battc.go
@@ -117,7 +117,7 @@ func handler() {
 			h := m.Get("h")
 			j := NewJob(h)
 			jobs[h] = j
-			go j.Build(m.Get("path"))
+			go j.Build(m.Get("path"), m.Get("platform"))
 		case "accept":
 			h := m.Get("h")
 			j, ok := jobs[h]
@@ -143,13 +143,19 @@ type Job struct {
 	filename string
 }
 
+func (j *Job) logf(format string, args ...interface{}) {
+	s := fmt.Sprintf(format, args...)
+	log.Printf("Job %s: %s", j.h, s)
+}
+
 func (j *Job) status(msg string) {
+	j.logf("%s", msg)
 	out <- batt.Message{"status", url.Values{
 		"h": []string{j.h}, "text": []string{msg},
 	}}
 }
 
-func (j *Job) Build(path string) {
+func (j *Job) Build(path, platform string) {
 	j.status("starting")
 	result := batt.Message{"result", url.Values{"h": []string{j.h}}}
 	build := func() error {
@@ -162,85 +168,100 @@ func (j *Job) Build(path string) {
 
 		j.status("fetching and building")
 		cmd := exec.Command("go", "get", path)
-		cmd.Env = env(gopath)
+		cmd.Env = env(gopath, platform)
 		if b, err := cmd.CombinedOutput(); err != nil {
-			return fmt.Errorf("%v\n%s", err, b)
+			return fmt.Errorf("go get: %v\n%s", err, b)
 		}
 
 		j.status("finding binary")
 		bindir := filepath.Join(gopath, "bin")
-		fi, err := findFile(bindir)
+		bin, size, err := findFile(bindir)
 		if err != nil {
-			return err
+			return fmt.Errorf("finding: %v", err)
 		}
-		bin := filepath.Join(bindir, fi.Name())
-		j.filename = fi.Name()
+		j.filename = filepath.Base(bin)
 		result.Set("filename", j.filename)
-		result.Set("size", fmt.Sprint(fi.Size()))
+		result.Set("size", fmt.Sprint(size))
 
 		j.status("hashing")
 		h, err := batt.ReadFileSHA1(bin)
 		if err != nil {
-			return err
+			return fmt.Errorf("hashing: %v", err)
 		}
 		result.Set("sha1", h)
 
 		j.status("storing file")
 		tmpfile, err := cpToTempFile(bin, h)
 		if err != nil {
-			return err
+			return fmt.Errorf("storing: %v", err)
 		}
 		j.tmpfile = tmpfile
 
 		return nil
 	}
 	if err := build(); err != nil {
+		j.logf("build: %v", err)
 		result.Set("err", err.Error())
 	}
-	log.Println(result)
-	log.Println("tmpfile:", j.tmpfile)
+	if j.tmpfile != "" {
+		j.logf("tmpfile: %s", j.tmpfile)
+	}
+	j.logf("result: %v", result)
 	out <- result
 
-	j.status("waiting for upload url")
+	j.status("waiting for accept")
 }
 
 func (j *Job) Accept(uploadUrl string) {
-	defer os.Remove(j.tmpfile)
 	defer j.status("done")
+	defer os.Remove(j.tmpfile)
+
 	j.status("uploading")
 	f, err := os.Open(j.tmpfile)
 	if err != nil {
-		log.Println(err)
+		j.logf("%v", err)
 		return
 	}
 	defer f.Close()
 	req, err := http.NewRequest("PUT", uploadUrl, f)
 	if err != nil {
-		log.Println(err)
+		j.logf("NeqRequest: %v", err)
 		return
 	}
 	res, err := http.DefaultTransport.RoundTrip(req)
 	if err != nil {
-		log.Println(err)
+		j.logf("RoundTrip: %v", err)
 		return
 	}
 	if res.StatusCode/100 != 2 {
-		log.Println("bad upload:", res.Status)
-		return
+		j.logf("HTTP Response: %v", res.Status)
 	}
 }
 
-func env(gopath string) []string {
+func env(gopath, platform string) []string {
+	p := strings.SplitN(platform, "-", 2)
+	var goos, goarch string
+	if len(p) > 1 {
+		goos, goarch = p[0], p[1]
+	}
 	s := os.Environ()
 	for i := len(s) - 1; i >= 0; i-- {
 		switch {
-		case strings.HasPrefix(s[i], "GOPATH="):
+		case strings.HasPrefix(s[i], "GOARCH="):
 		case strings.HasPrefix(s[i], "GOBIN="):
+		case strings.HasPrefix(s[i], "GOOS="):
+		case strings.HasPrefix(s[i], "GOPATH="):
 		default:
 			continue
 		}
 		s[i] = s[len(s)-1]
 		s = s[:len(s)-1]
+	}
+	if goos != "" {
+		s = append(s, "GOOS="+goos)
+	}
+	if goarch != "" {
+		s = append(s, "GOARCH="+goarch)
 	}
 	return append(s, "GOPATH="+gopath)
 }
@@ -263,18 +284,23 @@ func cpToTempFile(filename, tmpfilename string) (tmpfile string, err error) {
 	return f.Name(), nil
 }
 
-func findFile(dir string) (os.FileInfo, error) {
+func findFile(dir string) (filename string, size int64, err error) {
 	d, err := os.Open(dir)
 	if err != nil {
-		return nil, err
+		return
 	}
 	defer d.Close()
 	fis, err := d.Readdir(0)
 	if err != nil {
-		return nil, err
+		return
 	}
 	if len(fis) < 1 {
-		return nil, errors.New("couldn't find file")
+		err = errors.New("couldn't find file")
+		return
 	}
-	return fis[0], nil
+	if fis[0].IsDir() {
+		// recurse into directories
+		return findFile(filepath.Join(dir, fis[0].Name()))
+	}
+	return filepath.Join(dir, fis[0].Name()), fis[0].Size(), nil
 }
